59555bc08f107d4c671e2b3b2950339c5b66d6f7
Yuli Wang <yuliwang1007@gmail.com>
2023-09-23 14:51:09 -0400

    complete exercise_6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Thomas", last_name: "Laporte", hourly_rate: 60)
+@store1.employees.create(first_name: "Pierrw", last_name: "Gasly", hourly_rate: 75)
+@store2.employees.create(first_name: "Regina", last_name: "Zhao", hourly_rate: 70)
+@store2.employees.create(first_name: "Yuki", last_name: "Tsunoda", hourly_rate: 75)
+@store2.employees.create(first_name: "Guanyu", last_name: "Zhou", hourly_rate: 65)
+
+puts Employee.count
--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,4 @@
 class Store < ActiveRecord::Base
   #define the Active Record model Store to be linked with stores table in the database
-
+  has_many :employees
 end
