749ddfba850ef332033f31231cd91d8c96c181d3
Yuli Wang <yuliwang1007@gmail.com>
2023-09-23 13:10:39 -0400

    complete exercise_4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,18 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true, womens_apparel: false)
+
+#puts "Here are mens stores #{@mens_stores}"
+#puts Store.count
+@mens_stores.each {|store| puts store.name, store.annual_revenue}
+
+@womens_store = Store.where(womens_apparel: true).where("annual_revenue < ?", 1000000)
+#can not use Ruby comparison directly in the .where AR method!, instead should use a String to specify the SQL conditions
+
+@womens_store.each{|store| puts store.name, store.annual_revenue}
+
